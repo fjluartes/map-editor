e958d8bea102709382a94aba166cff6f06caccf5
Frederik Luartes <fjluartes@gmail.com>
2026-07-06 14:41:23 +0800

    Edits build scripts

--- build.bat
+++ build.bat
@@ -1,6 +1,28 @@
 @echo off
+setlocal
+
 cmake --preset windows-default
 cmake --build --preset windows-default
-for /f "delims=" %%A in ('some_command') do (
-    build\map-editor.exe %%A
+
+if "%~2"=="" (
+    echo Usage: %~nx0 {-edit^-|-map} ^<map_name^>
+    exit /b 1
 )
+
+if not "%~3"=="" (
+    echo Usage: %~nx0 {-edit^-|-map} ^<map_name^>
+    exit /b 1
+)
+
+set "MODE=%~1"
+set "MAP_NAME=%~2"
+
+if /I "%MODE%"=="-edit" goto run
+if /I "%MODE%"=="-map" goto run
+
+echo Error: first argument must be -edit or -map
+echo Usage: %~nx0 {-edit^-|-map} ^<map_name^>
+exit /b 1
+
+:run
+build\map-editor.exe "%MODE%" "%MAP_NAME%"
--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 cmake --preset unix-default
 cmake --build --preset unix-default
-while read arg; do
-  ./build/map-editor "$arg"
-done
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 {-edit|-map} <map_name>"
+    exit 1
+fi
+
+MODE="$1"
+MAP_NAME="$2"
+
+case "$MODE" in
+    -edit|-map)
+        ./build/map-editor "$MODE" "$MAP_NAME"
+        ;;
+    *)
+        echo "Error: first argument must be -edit or -map"
+        echo "Usage: $0 {-edit|-map} <map_name>"
+        exit 1
+        ;;
+esac
